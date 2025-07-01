bbe84a300f82fc61caefdee0b109514a0a55cf2f
MheePooh <sophonjampasornklin@gmail.com>
2025-07-01 22:57:41 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className=" flex flex-col h1 items-center justify-center min-h-screen bg-gray-100">
+    
+      <p className=" h1  mt-4 text-lg text-gray-700">
+        This is a simple example of a Next.js application with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
