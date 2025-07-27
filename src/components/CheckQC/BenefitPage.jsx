0130ee23e2cb3a0168488de2f2c6c96b158221af
MheePooh <sophonjampasornklin@gmail.com>
2025-07-27 13:04:57 +0700

    part 2

--- src/components/CheckQC/BenefitPage.jsx
+++ src/components/CheckQC/BenefitPage.jsx
@@ -7,6 +7,9 @@ import side_paper from "assets/images/side_paper.png";
 import prefix_blue from "assets/images/prefix_blue.png";
 import paper_01 from "assets/images/paper_01.png";
 import paper_02 from "assets/images/paper_02.png";
+import paper_03 from "assets/images/paper_03.png";
+import paper_04 from "assets/images/paper_04.png";
+import paper_frame from "assets/images/paper_frame.png";
 
 const BenefitPage = () => {
   return (
@@ -117,6 +120,124 @@ const BenefitPage = () => {
             <br /> ซึ่งเป็นสิ่งจำเป็นอย่างมากในกระบวนการ
             <br />
             ประชาธิไตย
+          </p>
+        </div>
+      </div>
+      <div className="max-w-[376px] mx-auto mt-10">
+        <Image src={paper_03} alt="Background" className=" w-full mt-2 mb-5" />
+        <p className=" b3 my-3">
+          <b>
+            การเปิดเผยข้อมูลและการเพิ่ม
+            <br /> การมีส่วนร่วมของประชาชนจะทำให้ กมธ.
+            <br />
+            ทำงานอยู่ภายใต้การตรวจสอบของ
+            <br /> ประชาชนและสื่อมวลชน
+          </b>{" "}
+          ซึ่งจะเป็น
+          <br /> แรงจูงใจให้ กมธ. ปฏิบัติหน้าที่ด้วย
+          <br /> ความรอบคอบ โปร่งใส และมีความรับผิด
+        </p>
+        <Image src={paper_04} alt="Background" className=" w-full mt-2 mb-5" />
+        <p className=" b3 my-3">
+          ดังนั้นแล้ว การเปิดเผยข้อมูลของ กมธ.
+          <br /> และการมีส่วนร่วมจากประชาชน
+          <br /> ไม่เพียงแต่ส่งเสริมความโปร่งใสและ
+          <br /> ความน่าเชื่อถือของกระบวนการ
+          <br /> นิติบัญญัติ{" "}
+          <b>
+            แต่ยังเป็นการรักษา
+            <br />
+            สิทธิของประชาชนในการรับรู้และมีส่วนร่วม
+            <br /> ในกระบวนการทางการเมืองเช่นกัน
+          </b>
+        </p>
+      </div>
+      <div className=" relative max-w-[501px] w-full py-20 px-7 bg-white mt-5 border border-[#2322BC]">
+        <Image
+          src={paper_frame}
+          alt="Background"
+          className=" full top-0 right-0 translate-y-[-55%] scale-[100.5%]  absolute"
+        />
+        <div className=" text-left flex space-x-2">
+          <Image src={paper_01} alt="Background" className=" w-[57px]" />
+          <div>
+            <p className=" b4 font-bold">รศ.ดร.วรรณภา ติระสังขะ</p>
+            <p className=" b5">อาจารย์ประจำคณะรัฐศาสตร์ </p>
+            <p className=" b5">มหาวิทยาลัยธรรมศาสตร์</p>
+          </div>
+        </div>
+        <div className="b3 text-left mt-10">
+          <Image
+            src={prefix_blue}
+            alt="Background"
+            className=" w-[38px] mt-2 mb-5"
+          />
+          <p>
+            การเพิ่มการมีส่วนร่วมจากประชาชนจะทำให้ความเป็นตัวแทนของนัก
+            <br />
+            การเมือง active (กระตือรือร้น) และ professional (มีความเป็น
+            <br />
+            มืออาชีพ) มากขึ้น และสามารถเปลี่ยนวิถีการทำงานของ สส. และ สว.ได้
+          </p>
+          <p className="mt-5">
+            เช่น ในมุมของ สส. เขต เขาสามารถแสดงออกได้ว่าเขาไม่ใช่ แค่ตัวแทน
+            <br />
+            เชิงพื้นที่เท่านั้น แต่เป็นตัวแทนของคนทั้งประเทศด้วย
+            เมื่อต้องผลักดัน
+            <br />
+            วาระเชิงนโยบายหรือตรวจสอบผ่านการทำงานของ กมธ. เช่น ประเด็น
+            <br />
+            กมธ. ที่ตรวจสอบสำนักงานประกันสังคม
+          </p>
+          <p className=" mt-5">
+            ทำให้ประชาชน (ที่เป็นฐานเสียง) เห็นว่าตัวแทนที่เลือกไปทำงาน
+            <br />
+            ตอบสนองนโยบายที่เคยหาเสียงหรือสัญญาไว้หรือไม่ หรือทำงาน
+            <br />
+            ได้มีประสิทธิภาพมากแค่ไหนการ
+          </p>
+          <p className="mt-5">
+            เปิดเผยข้อมูลจะช่วยให้เห็นได้ว่า สส. ทำงานมีประสิทธิภาพไหม
+            <br />
+            และข้อดีคือ ประชาชนจะรู้ภาระงานของนักการเมืองที่ทำงาน แล้ว
+            <br />
+            จะมีตัวเลือกตัวแทนของตนในการเลือกตั้งครั้งหน้าได้
+          </p>
+        </div>
+      </div>
+      <div className=" relative max-w-[501px] w-full py-20 px-7 bg-white mt-10 border border-[#2322BC]">
+        <Image
+          src={paper_frame}
+          alt="Background"
+          className=" full top-0 right-0 translate-y-[-55%] scale-[100.5%]  absolute"
+        />
+        <div className=" text-left flex space-x-2">
+          <Image src={paper_02} alt="Background" className=" w-[57px]" />
+          <div>
+            <p className=" b4 font-bold">เทวฤทธิ์ มณีฉาย </p>
+            <p className=" b5">สมาชิกวุฒิสภา </p>
+          </div>
+        </div>
+        <div className="b3 text-left mt-10">
+          <Image
+            src={prefix_blue}
+            alt="Background"
+            className=" w-[38px] mt-2 mb-5"
+          />
+          <p>
+            การทำงานของ กมธ. จะได้รับมุมมองในหลายมิติมากขึ้น และถ้า
+            <br />
+            มีข้อผิดพลาดจะได้แก้ไขทันท่วงที 
+          </p>
+          <p className="mt-5">
+            การเปิดข้อมูลที่มีคุณภาพและการเปิดพื้นที่การมีส่วนร่วมให้ประชาชน
+            <br /> จะทำให้ กมธ. ได้ข้อมูลที่มันรอบคอบ รอบด้านและผ่านการพิสูจน์ 
+          </p>
+          <p className=" mt-5">
+            Performance (ผลงาน) ของ กมธ. จะดีขึ้นด้วย ผ่านกลไกตรวจสอบ
+            <br />
+            กำกับ ควบคุม และถอดถอนจากสาธารณะ
+            <br />
           </p>
         </div>
       </div>
