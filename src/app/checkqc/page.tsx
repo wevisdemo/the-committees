74ff5bc1dbeb1722b3d7572ddfe1c895b01e03ef
MheePooh <sophonjampasornklin@gmail.com>
2025-09-07 20:30:54 +0700

    update check qc

--- src/app/checkqc/page.tsx
+++ src/app/checkqc/page.tsx
@@ -1,10 +1,10 @@
-import CommiteeOpenData from "@/components/checkqc/CommiteeOpenData";
-import CommiteeSurvey from "@/components/checkqc/CommiteeSurvey";
-import IntroLandingPage from "@/components/checkqc/IntroLandingPage";
-import LastPage from "@/components/checkqc/LastPage";
-import SurveyPage from "@/components/checkqc/SurveyPage";
+import CommiteeOpenData from "@/components/checkqcc/CommiteeOpenData";
+import CommiteeSurvey from "@/components/checkqcc/CommiteeSurvey";
+import IntroLandingPage from "@/components/checkqcc/IntroLandingPage";
+import LastPage from "@/components/checkqcc/LastPage";
+import SurveyPage from "@/components/checkqcc/SurveyPage";
 import React from "react";
-import BenefitPage from "@/components/checkqc/BenefitPage";
+import BenefitPage from "@/components/checkqcc/BenefitPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
